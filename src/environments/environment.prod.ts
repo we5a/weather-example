e7341f987c83ca358313144441416d0f345de564
we5a <47641684+we5a@users.noreply.github.com>
2019-12-03 18:45:49 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://api.weatherbit.io/v2.0/',
+  apiKey: '468d07037a5a4331b543b2a5fd6f27e3',
+  geocodingKey: 'AIzaSyAaDUv-mKUso80C3NUT13obbDFWTTR8DPA'
 };
